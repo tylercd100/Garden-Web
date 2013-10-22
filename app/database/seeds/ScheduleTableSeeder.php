ad6a43b48aa3fc1e184ec692412052d1c07c4cc8
tylercd100 <tylercd100@gmail.com>
2013-10-21 20:15:36 -0600

    asdf

--- app/database/seeds/ScheduleTableSeeder.php
+++ app/database/seeds/ScheduleTableSeeder.php
@@ -13,7 +13,7 @@ class ScheduleTableSeeder extends Seeder {
 
         Schedule::create(array('device_id' => 1, 'hour' =>  9, 'duration' => 60*60*12));
         Schedule::create(array('device_id' => 2, 'hour' =>  0, 'duration' => 60*60*12));
-        Schedule::create(array('device_id' => 3, 'day' => 'Mon', 'hour' => 10, 'duration' => 5));
+        Schedule::create(array('device_id' => 3, 'day' => 'Mon', 'hour' => 20, 'duration' => 3600));
         Schedule::create(array('device_id' => 3, 'day' => 'Wed', 'hour' => 10, 'duration' => 5));
         Schedule::create(array('device_id' => 3, 'day' => 'Fri', 'hour' => 10, 'duration' => 5));
     }
