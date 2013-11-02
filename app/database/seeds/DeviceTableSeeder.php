bc26edb2b519b1081f4b70c13f421562751f6ff2
Tyler <tyler@ubuntu.ubuntu-domain>
2013-11-02 17:12:34 -0600

    updated seeder to populate with proper devices for greenhouse

--- app/database/seeds/DeviceTableSeeder.php
+++ app/database/seeds/DeviceTableSeeder.php
@@ -11,10 +11,15 @@ class DeviceTableSeeder extends Seeder {
     {
         DB::table('devices')->delete();
 
-        Device::create(array('pin'=>6, 'name' => 'Top Shelf', 'type' => 'light','state' => '1'));
-        Device::create(array('pin'=>7, 'name' => 'Middle Shelf', 'type' => 'light','state' => '0'));
-        Device::create(array('pin'=>8, 'name' => 'Top Shelf', 'type' => 'pump','state' => '0', 'max_on' => 3000));
-    }
+        Device::create(array('pin'=>5, 	'name' => 'Light 1', 	'type' => 'light',	'state' => '1'));
+        Device::create(array('pin'=>6, 	'name' => 'Ground Fan', 'type' => 'fan',	'state' => '1'));
+        Device::create(array('pin'=>7, 	'name' => 'Heater', 	'type' => 'heater',	'state' => '1'));
+        Device::create(array('pin'=>8, 	'name' => 'Light 2', 	'type' => 'light',	'state' => '1'));
+        Device::create(array('pin'=>9, 	'name' => 'Light 3', 	'type' => 'light',	'state' => '1'));
+        Device::create(array('pin'=>10, 'name' => 'Window 1', 	'type' => 'motor',	'state' => '1'));
+        Device::create(array('pin'=>11, 'name' => 'Window 2', 	'type' => 'motor',	'state' => '1'));
+        Device::create(array('pin'=>12, 'name' => 'Window 3', 	'type' => 'motor',	'state' => '1'));
+    }	
 
 
 }
