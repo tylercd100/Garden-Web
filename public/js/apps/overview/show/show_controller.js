1d184bd1cb9a72b87a664ff9b4fda6cd52c52f63
tylercd100 <tylercd100@gmail.com>
2013-11-26 01:36:23 -0700

    interval updates

--- public/js/apps/overview/show/show_controller.js
+++ public/js/apps/overview/show/show_controller.js
@@ -18,20 +18,36 @@ App.module("Overview.Show", function(Show, App, Backbone, Marionette, $, _){
 					_.each(_sensors,function(s){
 						console.log('here')
 						var v = new Show.Sensor({model: s});
+						v.model.on('change reset',v.render,v);
 						view.$el.find('.window.'+location).append(v.render().el);
 					});
 
 					_.each(_devices,function(d){
 						console.log('here')
 						var v = new Show.Device({model: d});
+						v.model.on('change reset',v.render,v);
 						view.$el.find('.window.'+location).append(v.render().el);
 					});
 				});
 
 				view.$el.find('.has-tooltip').tooltip({placement:'bottom'});
+
+				refresh();
+
 			});
 
-			
+			function refresh(){
+				var devices = App.request("entities:device");
+				var sensors = App.request("entities:sensor");
+				var timeout;
+				$.when(devices,sensors).then(function(){
+					timeout = setTimeout(refresh, 5000);
+					view.on('close',function(){
+						clearTimeout(timeout);
+					});
+				})
+				
+			};
 		}
 	};
 });
