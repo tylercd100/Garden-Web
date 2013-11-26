e937eb5e04252b7557bafc1bf8a2c3374285b032
Tyler <tyler@ubuntu.ubuntu-domain>
2013-11-25 19:57:42 -0700

    fixed the seeder conflicts

--- app/database/seeds/ConditionTableSeeder.php
+++ app/database/seeds/ConditionTableSeeder.php
@@ -11,11 +11,11 @@ class ConditionTableSeeder extends Seeder {
     {
         DB::table('conditions')->delete();
 
-<<<<<<< HEAD
-        Condition::create(array('type'=>'value','device_id' => 1,'sensor_id' => 1,'operator' => '<','value' => 70)); //if inside temperature is less than 71 turn on light one
-=======
+        // My Setup
+        // Condition::create(array('type'=>'value','device_id' => 1,'sensor_id' => 1,'operator' => '<','value' => 70)); //if inside temperature is less than 71 turn on light one
+        
+        // My Dad's Setup
         // Condition::create(array('type'=>'value','device_id' => 1,'sensor_id' => 1,'operator' => '>','value' => 72)); //if inside temperature is less than 71 turn on light one
->>>>>>> 74a1ab649a5d72c82d5175c571b753290c21a3fc
         // Condition::create(array('type'=>'sensor','device_id' => 2,'sensor_id' => 1,'operator' => '>','comp_sensor_id' => 2)); //if inside temperature sensor is less than outside sensor turn on heater
         // Condition::create(array('type'=>'sensor','device_id' => 3,'sensor_id' => 1,'operator' => '<','comp_sensor_id' => 2)); //if inside temperature sensor is less than outside sensor turn on heater
         // Condition::create(array('type'=>'sensor','device_id' => 4,'sensor_id' => 1,'operator' => '>','comp_sensor_id' => 2)); //if inside temperature sensor is less than outside sensor turn on heater
--- app/database/seeds/DeviceTableSeeder.php
+++ app/database/seeds/DeviceTableSeeder.php
@@ -11,12 +11,12 @@ class DeviceTableSeeder extends Seeder {
     {
         DB::table('devices')->delete();
 
-        Device::create(array('pin'=>5,'location' => 'inside',   'name' => 'Heater',    'type' => 'heater', 'state' => 0));
-        
-        // Device::create(array('pin'=>6,'location' => 'inside',   'name' => 'Light 1',    'type' => 'light', 'state' => 0));
-        // Device::create(array('pin'=>7,'location' => 'inside',   'name' => 'Light 2',    'type' => 'light', 'state' => 0));
-        // Device::create(array('pin'=>8,'location' => 'inside',   'name' => 'Pump 1',     'type' => 'pump', 'state' => 0));
+        // My Setup
+        Device::create(array('pin'=>6,'location' => 'inside',   'name' => 'Light 1',    'type' => 'light', 'state' => 0));
+        Device::create(array('pin'=>7,'location' => 'inside',   'name' => 'Light 2',    'type' => 'light', 'state' => 0));
+        Device::create(array('pin'=>8,'location' => 'inside',   'name' => 'Pump 1',     'type' => 'pump', 'state' => 0));
 
+        // My Dad's Setup
         // Device::create(array('pin'=>5,  'name' => 'Light 1',    'type' => 'light',  'state' => 0));
         // Device::create(array('pin'=>6,  'name' => 'Ground Fan', 'type' => 'fan',    'state' => 0));
         // Device::create(array('pin'=>7,  'name' => 'Heater',     'type' => 'heater', 'state' => 0));
